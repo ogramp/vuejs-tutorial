8d83ecce0b9159ca76a3b7378509f7aa78f9715b
Margo Pruul <margo.pruul@gmail.com>
2017-08-31 21:29:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ title }} - 19 is the next one!</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Your first Vue file, wooo!',
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'heeey cowboy!';
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+</style>
