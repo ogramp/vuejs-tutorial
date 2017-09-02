ad14715b55887a252128d5b948acb2f643b1294f
Margo Pruul <margo.pruul@gmail.com>
2017-09-02 10:43:37 +0300

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
 	<div>
 		<app-header></app-header>
+		<app-footer></app-footer>
 	</div>
 </template>
 
 <script>
 // Import the component.
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
 	// Register the component locally.
 	components: {
-		'app-header': Header
+		'app-header': Header,
+		'app-footer': Footer
 	},
 	data () {
 		return {
