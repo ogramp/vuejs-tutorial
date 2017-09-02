eaa33c5f569b949d715f52c0130bcedefe3feaa3
Margo Pruul <margo.pruul@gmail.com>
2017-09-02 10:36:55 +0300

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-
+	<div>
+		<app-header></app-header>
+	</div>
 </template>
 
 <script>
+// Import the component.
+import Header from './components/Header.vue'
+
 export default {
+	// Register the component locally.
+	components: {
+		'app-header': Header
+	},
 	data () {
 		return {
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Ninjas from './Ninjas.vue'
-
-// Registering vue component globally
-// Vue.component('ninjas', Ninjas);
 
 new Vue({
   el: '#app',
