74fd6851321913592179ecb856d42c1ba5d84702
Margo Pruul <margo.pruul@gmail.com>
2017-09-02 10:30:16 +0300

    set tutorial starting poing

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
-  </div>
+
 </template>
 
 <script>
-// Registering component locally.
-import Ninjas from './Ninjas.vue'
-
 export default {
-  components: {
-    'ninjas': Ninjas
-  },
-  data () {
-    return {
-      title: 'Ninja App',
-    }
-  }
+	data () {
+		return {
+
+		}
+	}
 }
 </script>
 
-<style scoped>
-
-h1 {
-  color: purple;
-}
+<style >
 
 </style>
