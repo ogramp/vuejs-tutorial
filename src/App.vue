54024ecae454d8a11dcbb295e507d80d0e99a3c5
Margo Pruul <margo.pruul@gmail.com>
2017-09-03 18:40:27 +0300

    input binding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
 	<div>
-
+		<add-blog></add-blog>
 	</div>
 </template>
 
 <script>
-
+import addBlog from './components/addBlog.vue';
 
 	export default {
-
+		components: {
+			'add-blog': addBlog
+		},
 		data () {
 			return {
 				
