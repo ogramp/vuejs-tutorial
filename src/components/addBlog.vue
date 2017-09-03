aae1d363facfa376b568b8b1377903ce33bf143a
Margo Pruul <margo.pruul@gmail.com>
2017-09-03 20:57:23 +0300

    add firebase

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -58,11 +58,7 @@
 		},
 		methods: {
 			post: function() {
-				this.$http.post('https://jsonplaceholder.typicode.com/posts', {
-					title: this.blog.title,
-					body: this.blog.content,
-					userId: 1
-				}).then(function(data) {
+				this.$http.post('https://nn-vue-playlis.firebaseio.com/posts.json', this.blog).then(function(data) {
 					console.log(data);
 					this.submitted = true;
 				});
